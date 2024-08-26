f07dbb19ef2752d81dd332e62b531d930cd13985
junjun932 <1004ji93@gmail.com>
2024-08-27 00:29:46 +0900

    "버튼수정"

--- src/assets/components/Dashboard.jsx
+++ src/assets/components/Dashboard.jsx
@@ -1,17 +1,19 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const DashboardContainer = styled.div``;
 
 const Dashboard = ({ seletedPokemon }) => {
   return (
     <DashboardContainer>
-      <h2>대시보드</h2>
+      <h2>들어가라 포켓몬 </h2>
       {seletedPokemon.length === 0 ? (
         <p>선택된 포켓몬이 없습니다.</p>
       ) : (
         <ul>
           {seletedPokemon.map((pokemon) => (
-            <li key={pokemon.id}>{pokemon.korean_name} </li>
+            <li key={pokemon.id}>
+              {pokemon.korean_name} {pokemon.removePokemon} {pokemon.isSelect}
+            </li>
           ))}
         </ul>
       )}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,17 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1>포켓몬 도감</h1>
-      <img src="pokedex.png" alt="pokedex-imgage" className="pokedex-image" />
-      <button onClick={() => navigate("/dex")}>포켓몬 도감 시작하기</button>
+    <div className="App container">
+      <div className="header">
+        <h1>포켓몬 도감</h1>
+        <img src="pokedex.png" alt="pokedex-imgage" className="pokedex-image" />
+        <button className="start-button" onClick={() => navigate('/dex')}>
+          포켓몬 도감 시작하기
+        </button>
+      </div>
     </div>
   );
 }
