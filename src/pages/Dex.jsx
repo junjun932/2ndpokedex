53851e63f4734b3a1a20dde71fa251e3fd7ced1f
junjun932 <1004ji93@gmail.com>
2024-08-27 01:51:33 +0900

    "중복메시지 추가"

--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,20 +1,24 @@
-import { useState } from "react";
-import MOCK_DATA from "../../mock";
-import Dashboard from "../assets/components/Dashboard";
-import PokemonList from "../assets/components/PokemonList";
+import { useState } from 'react';
+import MOCK_DATA from '../../mock';
+import Dashboard from '../assets/components/Dashboard';
+import PokemonList from '../assets/components/PokemonList';
 
 //selectedPokemon =삳태 , setSelectedPokemon=상태변경함수 , useState(초기값)
 function Dex() {
   const [selectedPokemon, setSelectedPokemon] = useState([]);
 
   const addPokemon = (pokemon) => {
+    if (selectedPokemon.some((list) => list.id === pokemon.id)) {
+      alert('중복된 포켓몬이 있습니다');
+      return;
+    }
     setSelectedPokemon([...selectedPokemon, pokemon]);
-    console.log(selectedPokemon);
+    // console.log(selectedPokemon);
   };
 
   const removePokemon = (pokemon) => {
     const updatedPokemonList = selectedPokemon.filter(
-      (item) => item.id !== pokemon.id
+      (list) => list.id !== pokemon.id
     );
     setSelectedPokemon(updatedPokemonList);
   };
